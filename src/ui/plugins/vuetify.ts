980d080455b9783c4d4141f07b8bbe7554be6b80
Kevin Bein <mail@kevinbein.de>
2020-03-31 03:13:12 +0200

    Add font configuration for vuetify

--- src/ui/plugins/vuetify.ts
+++ src/ui/plugins/vuetify.ts
@@ -1,18 +1,23 @@
 import Vue from 'vue';
 // either load vuetify components a-la-carte
-import Vuetify, { VBtn, VApp } from 'vuetify/lib';
+//import Vuetify, { VBtn, VApp } from 'vuetify/lib';
 
 // or load all of Vuetify
-// import Vuetify from 'vuetify';
-// import 'vuetify/dist/vuetify.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+//import '@mdi/font/css/materialdesignicons.css'
+
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
-  components: {
-    VBtn,
-    VApp,
-  },
+  //components: {
+    ////VBtn,
+    ////VApp,
+  //},
 });
 
-const opts = {};
-
-export const vuetify = new Vuetify(opts);
+export const vuetify = new Vuetify({
+  icons: {
+    iconfont: 'fa',
+  },
+});
