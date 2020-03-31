1974da5cbd1fd2b973db586d03319b13e3d76cd1
Kevin Bein <mail@kevinbein.de>
2020-03-31 03:14:22 +0200

    Add landing page

--- src/ui/pages/landing/landing.tsx
+++ src/ui/pages/landing/landing.tsx
@@ -1,10 +1,32 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import Styles from './landing.scss';
+//import { NavBar } from './../../components/navBar/navBar';
 
 @Component({
   name: 'Landing',
-  render(): Vue.VNode {
-    return <main>Landing Page</main>;
+  render(h): Vue.VNode {
+    return (
+      <div class={Styles['landing-page']}>
+
+        {/*<NavBar></NavBar>*/}
+
+        <div class={Styles['logo-container']}>
+          <img class={Styles['logo']} src='./assets/imgs/Herz_Logo_negativ.png' />
+        </div>
+
+        <div class={Styles['button-container']}>
+          {/*<p class={Styles['caption']}>DIREKT ZU DIR</p>*/}
+          <div class={Styles['button']}>
+            <router-link to="explore">STANDORT FREIGEBEN</router-link>
+          </div>
+          <div class={Styles['other-text']}> oder </div>
+          <div class={Styles['button']}>
+            <router-link to="citycode">POSTLEITZAHL EINGEBEN</router-link>
+          </div>
+        </div>
+      </div>
+    );
   },
 })
 export class LandingPage extends Vue {}
