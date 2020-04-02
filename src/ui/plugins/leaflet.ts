827ac043033e1f6ec228b2e0b3b63ea9275eb469
Kevin Bein <mail@kevinbein.de>
2020-04-02 17:42:31 +0200

    Add map page

---
+++ src/ui/plugins/leaflet.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+export const leaflet = {
+  name: 'MyAwesomeMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+};
