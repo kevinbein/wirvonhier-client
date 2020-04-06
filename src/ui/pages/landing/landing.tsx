6af5cca58d99d1a1007d6c28b76f150134bb1f2a
Antonio Lombardi <A.Lombardi013@gmail.com>
2020-04-06 16:31:07 +0200

    add_text_input_for_postcode

--- src/ui/pages/landing/landing.tsx
+++ src/ui/pages/landing/landing.tsx
@@ -9,7 +9,24 @@ export class LandingPage extends Vue {
 
   public gettingLocation = false;
   public location: Position | undefined;
+  private postCode = 'no';
   public errorStr = '';
+
+  // mounted(): void {
+  //   this.getPostcode();
+  // }
+
+  public getPostcode(): string {
+    if (window.localStorage.postCode) {
+      this.postCode = window.localStorage.postCode;
+    }
+    return this.postCode;
+  }
+
+  public savePostCode(): void {
+    window.localStorage.postCode = this.postCode;
+    this.$router.push('Explore');
+  }
 
   public async getLocation(): Promise<Position> {
     return new Promise((resolve, reject) => {
@@ -68,9 +85,17 @@ export class LandingPage extends Vue {
             <v-icon class={Styles['icon']}>fa-location-arrow</v-icon> AKTUELLER STANDORT
           </div>
           <div class={Styles['other-text']}> oder </div>
-          <div class={Styles['button']}>
-            <router-link to="explore">POSTLEITZAHL EINGEBEN</router-link>
-          </div>
+          <v-text-field
+            class={Styles['text-input']}
+            color="#5f6daf"
+            label="POSTLEITZAHL EINGEBEN"
+            change={(postcode: { target: { value: string } }) => (this.postCode = postcode.target.value)}
+          ></v-text-field>
+          <button onClick={this.savePostCode.bind(this)} class={Styles['search-button']}>
+            <v-icon class={Styles['search-icon']} size="30px">
+              fa-search
+            </v-icon>
+          </button>
         </div>
       </div>
     );
