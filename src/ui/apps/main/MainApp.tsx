e5e4fefcb09b5d8a31ed8a1140f370fe9965be91
MarcRoemmelt <mail@m-roemmelt.com>
2020-04-12 15:19:03 +0200

    feat: add dynamic vh setting for mobile (values vary due to differen browser handling)

--- src/ui/apps/main/MainApp.tsx
+++ src/ui/apps/main/MainApp.tsx
@@ -6,6 +6,19 @@ import { VirtualMobile } from '@/ui/components';
   name: 'Main',
 })
 export class MainApp extends Vue {
+  created(): void {
+    this.setVH();
+  }
+
+  setVH(): void {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
   // @ts-ignore: Declared variable is not read
   render(h): Vue.VNode {
     return (
