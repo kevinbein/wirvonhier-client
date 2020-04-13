43ad6731b923527811a92fc3c0eca7f636a5a066
MarcRoemmelt <mail@m-roemmelt.com>
2020-04-13 17:09:12 +0200

    feat: update http-service to be its own class

--- src/services/http/http.ts
+++ src/services/http/http.ts
@@ -1,14 +1,43 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { IHttpResponse } from './http.types';
+import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
+import { IHttpResponse, IQuery } from './http.types';
 
-const axiosInstance = axios.create({
+const withAuth = axios.create({
   timeout: 3000,
   baseURL: API_URL,
   withCredentials: true,
   responseType: 'json',
 });
 
-export const http = async (RequestOptions: AxiosRequestConfig): Promise<IHttpResponse> => {
-  const { data } = await axiosInstance.request(RequestOptions);
-  return data;
-};
+export class HTTP {
+  private withAuth: AxiosInstance;
+
+  constructor(withAuth: AxiosInstance) {
+    this.withAuth = withAuth;
+  }
+
+  async get(url: string, options?: AxiosRequestConfig): Promise<IHttpResponse> {
+    const res = await this.withAuth.get(url, options);
+    return res.data;
+  }
+
+  public constructQueryString(query: IQuery): string {
+    const queryArray = [];
+    if (query.limit) queryArray.push(`limit=${query.limit}`);
+    if (query.page) queryArray.push(`page=${query.page}`);
+    if (query.filters) {
+      for (const filter of query.filters) {
+        let value = filter.value;
+        value = value instanceof Array ? value.join('|') : value;
+        value =
+          typeof value === 'object' && filter.name === 'location'
+            ? `${value.zip ? value.zip : value.lng + ',' + value.lat},${value.maxDistance}`
+            : value;
+
+        queryArray.push(`filter_${filter.name}=${filter.operator ? filter.operator + ':' : ''}${value}`);
+      }
+    }
+    return queryArray.join('&');
+  }
+}
+
+export const http = new HTTP(withAuth);
--- src/services/http/http.types.ts
+++ src/services/http/http.types.ts
@@ -1,4 +1,24 @@
 export interface IHttpResponse {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: { [key: string]: any };
+  [key: string]: any;
+}
+
+export interface IQuery {
+  [key: string]: any;
+  limit?: number;
+  page?: number;
+  filters: IFilter[];
+}
+
+export interface IFilter {
+  name: string;
+  operator?: string;
+  value: string | string[] | number | number[] | ILocationFilter;
+}
+
+export interface ILocationFilter {
+  lng?: number;
+  lat?: number;
+  zip?: string;
+  maxDistance: number;
 }
