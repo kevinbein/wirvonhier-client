8d3b9a5adf23789e2b4d4d33a753ab495cf6f361
Alessandro W <a.wollek@snapaddy.com>
2020-04-29 20:59:10 +0200

    style:
    fix spacing and remove empty styles.scc

--- src/ui/apps/main/legal/termsOfUse/termsOfUse.tsx
+++ src/ui/apps/main/legal/termsOfUse/termsOfUse.tsx
@@ -7,12 +7,13 @@ export class TermsOfUsePage extends Vue {
   // @ts-ignore: Declared variable is not read
   render(h: CreateElement): Vue.VNode {
     return (
-    <div div class={Styles['legal']}>
-      <div class={Styles['legal__title']}>Fehlt noch</div>
-      <router-link to="/" class={Styles['close-button']}>
-        <v-icon class={Styles['icon']}>fa-times</v-icon>
-      </router-link>
-    </div>);
+      <div div class={Styles['legal']}>
+        <div class={Styles['legal__title']}>Fehlt noch</div>
+        <router-link to="/" class={Styles['close-button']}>
+          <v-icon class={Styles['icon']}>fa-times</v-icon>
+        </router-link>
+      </div>
+    );
   }
 }
 
