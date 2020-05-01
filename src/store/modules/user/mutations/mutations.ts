3cd15ecf349c77cafd0ca655051e0ab5495c2104
MarcRoemmelt <mail@m-roemmelt.com>
2020-05-01 15:07:12 +0200

    feat: userModule -> save date-fields as Date object

--- src/store/modules/user/mutations/mutations.ts
+++ src/store/modules/user/mutations/mutations.ts
@@ -6,6 +6,11 @@ import Vue from 'vue';
 export class UserDataMutations extends Mutations<UserDataState> {
   SET_USER_DATA(userData: Partial<IUserData>): void {
     Object.keys(userData).forEach((key) => {
+      if (['createdAt'].includes(key)) {
+        const date = isNaN(new Date(userData[key] as Date).getTime()) ? 0 : new Date(userData[key] as Date);
+        Vue.set(this.state, key, date);
+        return;
+      }
       Vue.set(this.state, key, userData[key]);
     });
   }
