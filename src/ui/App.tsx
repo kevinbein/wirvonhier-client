def0ec19a06c5528c78b72dcecdb4d9b5aa5f7d6
MarcRoemmelt <mail@m-roemmelt.com>
2020-05-01 15:12:21 +0200

    feat: some cleanup, minor fixes, additional image resolution

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,49 +2,15 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import '@/ui/styles/global/index.scss';
 
-import { IBusinessData } from '@/entities';
-import { CreateElement } from 'vue/types/umd';
-// Check ui/styles/global for directly used classes
-
 @Component({
   name: 'App',
 })
 export class App extends Vue {
-  get businesses(): IBusinessData[] {
-    return this.$store.state.Business.businesses;
-  }
-
-  get names(): string {
-    return this.businesses ? this.businesses.map((b) => b.name).join(' ') : 'nothing found';
-  }
-
-  private iosChangeAppBarStyle(_style: string): void {
-    // not working, but I leave it in for now. Maybe there is a way to trigger this change
-    // in ios pwas
-    return;
-    /*const el = document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']");
-    if (el !== null) {
-      el.setAttribute('content', style);
-    }*/
-  }
-
-  created(): void {
-    // @ts-ignore: Declared variable is not read
-    //const ctx = BusinessModule.context(this.$store);
-    //ctx.actions.loadBusinesses();
-
-    this.$root.$on('iosChangeAppBarStyle', (style: string) => this.iosChangeAppBarStyle(style));
-  }
-
   // @ts-ignore: Declared variable is not read
-  render(h: CreateElement): Vue.VNode {
+  render(h): Vue.VNode {
     return (
       <v-app>
-        {/*<div class={Styles['wvh-app-container']}>
-          <div class={Styles['wvh-app']}>*/}
         <router-view></router-view>
-        {/*</div>
-        </div>*/}
       </v-app>
     );
   }
