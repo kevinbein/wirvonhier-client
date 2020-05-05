842a775697e914f4cbf21b55fbf56e00420f7b14
MarcRoemmelt <mail@m-roemmelt.com>
2020-05-05 22:39:07 +0200

    fix/chore: improve typing; correct variable names and improve result handling in validateImgeUpload() action

--- src/store/modules/business/actions/actions.types.ts
+++ src/store/modules/business/actions/actions.types.ts
@@ -1,7 +1,13 @@
 import { Business } from '@/entities';
+import { IHttpSuccessResponse, IHttpErrorResponse } from '@/services';
+import { ICloudinaryImageUploadResponse } from '@/services/images/imageService.types';
 
 export interface IBusinessUpdateOptions {
   business: Business;
   key: string;
   value: unknown;
 }
+
+export type IUploadImagesResult =
+  | IHttpSuccessResponse<ICloudinaryImageUploadResponse>
+  | IHttpErrorResponse<ICloudinaryImageUploadResponse>;
