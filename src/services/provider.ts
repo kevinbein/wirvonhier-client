bbabac534e84bb3bba5b48fb3b83e39316b38935
MarcRoemmelt <mail@m-roemmelt.com>
2020-05-05 22:03:48 +0200

    feat: add image-service to upload images to cloudinary

--- src/services/provider.ts
+++ src/services/provider.ts
@@ -1,5 +1,6 @@
 import { IProvider } from './provider.types';
 import { BusinessService } from './business';
+import { ImagesService } from './images';
 import { DB } from '@/services/db';
 import { HTTP } from './http';
 import { IStore } from '@/store';
@@ -7,5 +8,6 @@ import { IStore } from '@/store';
 export function provider(store: IStore, worker: unknown, db: DB, http: HTTP): IProvider {
   return {
     business: new BusinessService(store, worker, db, http),
+    images: new ImagesService(store, worker, db, http),
   };
 }
--- src/services/provider.types.ts
+++ src/services/provider.types.ts
@@ -1,9 +1,11 @@
 import { BusinessService } from './business';
+import { ImagesService } from './images';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface IProvider {
   [key: string]: any;
   business: BusinessService;
+  images: ImagesService;
 }
 
 export interface IProviderMock {
