4e70ed5760822529a4a72b060aa559c588d8070b
Kevin Bein <mail@kevinbein.de>
2020-06-06 19:30:50 +0200

    slideInPage: Fill complete page and close when clicking above the slider. This prevents background scrolling like on the explore page

--- src/ui/components/slideInPage/slideInPage.tsx
+++ src/ui/components/slideInPage/slideInPage.tsx
@@ -58,34 +58,39 @@ export class SlideInPage extends VueComponent<IProps, IRefs> {
   }
 
   private swiping: Swipe | null = null;
-  public startSwiping(e: TouchEvent): void {
-    const touchObj = e.changedTouches[0];
+  public startSwiping(e: TouchEvent | MouseEvent): void {
+    this.stopEvents(e);
+
+    const eventData = e instanceof MouseEvent ? e : e.changedTouches[0];
     this.swiping = {
-      startY: touchObj.pageY,
-      lastY: touchObj.pageY,
+      startY: eventData.pageY,
+      lastY: eventData.pageY,
       lastDiff: 0,
       startTime: new Date().getTime(),
     };
-    //e.preventDefault();
   }
 
-  public moveSwiping(e: TouchEvent): void {
+  public moveSwiping(e: TouchEvent | MouseEvent): void {
+    this.stopEvents(e);
+
     if (this.swiping === null) {
       return;
     }
-    const touchObj = e.changedTouches[0];
-    this.swiping.lastDiff = touchObj.pageY - this.swiping.lastY;
-    this.swiping.lastY = touchObj.pageY;
-    const deltaY = this.swiping.lastY > this.swiping.startY ? this.getSwipeDiffY(touchObj) : 0;
+    const eventData = e instanceof MouseEvent ? e : e.changedTouches[0];
+    this.swiping.lastDiff = eventData.pageY - this.swiping.lastY;
+    this.swiping.lastY = eventData.pageY;
+    const deltaY = this.swiping.lastY > this.swiping.startY ? this.getSwipeDiffY(eventData) : 0;
     this.translateY = deltaY;
   }
 
-  public endSwiping(e: TouchEvent): void {
+  public endSwiping(e: TouchEvent | MouseEvent): void {
+    this.stopEvents(e);
+
     if (this.swiping === null) {
       return;
     }
-    const touchObj = e.changedTouches[0];
-    const diffY = this.getSwipeDiffY(touchObj);
+    const eventData = e instanceof MouseEvent ? e : e.changedTouches[0];
+    const diffY = this.getSwipeDiffY(eventData);
     if (diffY > 0) {
       if (this.swiping.lastDiff >= 0) {
         this.$emit('close', true);
@@ -95,20 +100,35 @@ export class SlideInPage extends VueComponent<IProps, IRefs> {
     this.swiping = null;
   }
 
+  public stopEvents(e: TouchEvent | MouseEvent): void {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  private getSwipeDiffY(eventData: Touch | MouseEvent): number {
+    if (this.swiping === null) {
+      return 0;
+    }
+    return eventData.pageY - this.swiping.startY;
+  }
+
   // @ts-ignore: Declared variable is not read
   render(h): Vue.VNode {
     const activeClass = this.value === true ? Styles['page--active'] : '';
     return (
       <div ref="page" class={`${Styles['page']} ${activeClass}`}>
-        <div style={{ transform: `translateY(${this.translateY}px)` }}>
+        {this.value === true && <div class={Styles['page__overlay']} onClick={this.close.bind(this)}></div>}
+        <div class={Styles['slider-container']} style={{ transform: `translateY(${this.translateY}px)` }}>
           <header
             ref="slider"
             class={Styles['slider']}
-            onTouchstart={(e: TouchEvent) => this.startSwiping(e)}
-            onTouchmove={(e: TouchEvent) => this.moveSwiping(e)}
-            onTouchend={(e: TouchEvent) => this.endSwiping(e)}
+            onMousedown={this.startSwiping.bind(this)}
+            onMousemove={this.moveSwiping.bind(this)}
+            onMouseup={this.endSwiping.bind(this)}
+            onTouchstart={this.startSwiping.bind(this)}
+            onTouchmove={this.moveSwiping.bind(this)}
+            onTouchend={this.endSwiping.bind(this)}
           >
-            <div class={Styles['slider__overlay']}></div>
             <div class={Styles['slider__indicator-container']}>
               <div class={Styles['slider__indicator']}></div>
             </div>
@@ -122,13 +142,6 @@ export class SlideInPage extends VueComponent<IProps, IRefs> {
         </div>
       </div>
     );
-  }
-
-  private getSwipeDiffY(touchObj: Touch): number {
-    if (this.swiping === null) {
-      return 0;
-    }
-    return touchObj.pageY - this.swiping.startY;
   }
 }
 
