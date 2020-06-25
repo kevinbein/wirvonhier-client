e59d5b49077214b840a24fbc2239c4193f161a2b
MarcRoemmelt <mail@m-roemmelt.com>
2020-06-25 13:05:29 +0200

    refactor: move VirtualMobile and setVH() to App component

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,13 +1,32 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { VirtualMobile } from '@/ui/components';
 import '@/ui/styles/global/index.scss';
 
 @Component({
   name: 'App',
 })
 export class App extends Vue {
+  created(): void {
+    this.setVH();
+  }
+
+  setVH(): void {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
   // @ts-ignore: Declared variable is not read
   render(h): Vue.VNode {
-    return <router-view></router-view>;
+    return (
+      <VirtualMobile>
+        <router-view></router-view>
+      </VirtualMobile>
+    );
   }
 }
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,4 +2,3 @@ export * from './App';
 export * from './router';
 export * from './plugins';
 export * from './components';
-export * from './vue';
