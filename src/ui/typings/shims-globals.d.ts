4a800916b255b60ea6be6716cf799d1e73a889eb
MarcRoemmelt <mail@m-roemmelt.com>
2020-07-02 14:51:54 +0200

    fix: remove global constant CLOUDINARY_FOLDER

--- src/ui/typings/shims-globals.d.ts
+++ src/ui/typings/shims-globals.d.ts
@@ -3,4 +3,3 @@ declare const API_URL: string;
 declare const CLOUD_NAME: string;
 declare const IMAGE_UPLOAD_URL: string;
 declare const CLOUDINARY_IMAGE_PRESET: string;
-declare const CLOUDINARY_FOLDER: string;
