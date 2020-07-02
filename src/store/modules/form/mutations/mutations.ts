965f5db353dd81715cd9fa021c6c5c9587264a68
MarcRoemmelt <mail@m-roemmelt.com>
2020-07-02 14:55:59 +0200

    fix: FormModule -> save new Map() to trigger vuex state updates

--- src/store/modules/form/mutations/mutations.ts
+++ src/store/modules/form/mutations/mutations.ts
@@ -1,14 +1,15 @@
 import { Mutations } from 'vuex-smart-module';
 import { FormState } from '../state';
 import { ISetFormDataPayload } from '../actions/actions.types';
+import Vue from 'vue';
 
 export class FormMutations extends Mutations<FormState> {
   UPDATE_FORM_DATA(payload: ISetFormDataPayload): void {
     const { formId, data } = payload;
     const form = this.state.formData.get(formId) || {};
     const updatedForm = Object.assign(form, data);
-    const newMap = this.state.formData;
+    const newMap = new Map(this.state.formData);
     newMap.set(formId, updatedForm);
-    this.state.formData = newMap;
+    Vue.set(this.state, 'formData', newMap);
   }
 }
