88fff5bf867933d34c073d1314b32c8acc34bfcf
MarcRoemmelt <mail@m-roemmelt.com>
2020-07-08 00:38:01 +0200

    fix: close RequestLocationToast when clicking a button

--- src/ui/components/toasts/requestLocationToast/RequestLocationToast.tsx
+++ src/ui/components/toasts/requestLocationToast/RequestLocationToast.tsx
@@ -6,16 +6,24 @@ import Styles from './requestLocationToast.scss';
   name: 'RequestLocationToast',
 })
 export class RequestLocationToast extends Vue {
+  public grantAccess(): void {
+    this.$emit('granted');
+    this.$emit('close-toast');
+  }
+  public denyAccess(): void {
+    this.$emit('denied');
+    this.$emit('close-toast');
+  }
   // @ts-ignore
   public render(h): Vue.VNode {
     return (
       <div class={Styles['request-location-toast']}>
         <div>Dürfen wir die GPS Koordinaten deines Gerätes abfragen?</div>
         <div class={Styles['request-location-toast__buttons']}>
-          <button class={Styles['request-location-toast__button']} on-click={() => this.$emit('granted')}>
+          <button class={Styles['request-location-toast__button']} on-click={this.grantAccess.bind(this)}>
             Ok
           </button>
-          <button class={Styles['request-location-toast__button']} on-click={() => this.$emit('denied')}>
+          <button class={Styles['request-location-toast__button']} on-click={this.denyAccess.bind(this)}>
             Abbrechen
           </button>
         </div>
